953b7b76c49d8d72e1ce1b4c24e0154cd8799710
Adam Merrill <adam.merrill@sebrands.com>
2015-11-03 07:38:45 -0600

    Meiot.Shell initial creation
    This is the first commit of the Meiot.Shell command line tool

---
+++ Meiot.Shell/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Meiot.Shell
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("let it begin");
+
+            var response = Console.ReadLine();
+        }
+    }
+}
