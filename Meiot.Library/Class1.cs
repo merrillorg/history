92af52c511c4be245e7462b31d4ded60572ad8e7
Adam Merrill <adam.merrill@sebrands.com>
2015-11-12 08:42:43 -0600

    Deleting test comments

--- Meiot.Library/Class1.cs
+++ Meiot.Library/Class1.cs
@@ -9,16 +9,10 @@ namespace Meiot.Library
 {
     public class Class1
     {
-        // comment here locally
-        // comment on github.com 3
-
         private readonly DataSet someDataSet;
         public Class1()
         {
-            //Adding this comment on github.com
             someDataSet = new DataSet();
-            
-            // Adding another comment on github.com
         }
     }
 }
